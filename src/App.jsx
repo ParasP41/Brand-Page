75f42519371e945a62d8e650bcbbd6b2c90868a1
ParasP41 <parasvishwakarma198@gmail.com>
2024-12-29 15:18:19 +0530

    brand-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,46 @@
 import React from 'react'
 import { initFlowbite } from 'flowbite'
+import { Navbar } from "flowbite-react";
 
 export default function App() {
   return (
     <div>
-      <nav className="bg-slate-200   w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="./src/assets/Nike.png" className="h-8" alt="Flowbite Logo" />
-          </a>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button type="button" className="text-white bg-red-600  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Login</button>
-            <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-              <span className="sr-only">Open main menu</span>
-              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-              </svg>
-            </button>
-          </div>
-          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
-              </li>
-              <li>
-                <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Location</a>
-              </li>
-              <li>
-                <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-              </li>
-              <li>
-                <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-              </li>
-            </ul>
-          </div>
+      <Navbar fluid rounded className='bg-slate-200'>
+        <Navbar.Brand href="/">
+          <img src="./src/assets/Nike.png" className="mr-3 h-6 sm:h-9" alt="" />
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Login</button>
+          <Navbar.Toggle />
         </div>
-      </nav>
+        <Navbar.Collapse>
+          <Navbar.Link href="/" className='md:text-[18px]'>Home</Navbar.Link>
+          <Navbar.Link href="/" className='md:text-[18px]'>Location</Navbar.Link>
+          <Navbar.Link href="/" className='md:text-[18px]'>About</Navbar.Link>
+          <Navbar.Link href="/" className='md:text-[18px]'>Contact</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
       <main className='md:flex md:gap-10 md:justify-center my-8'>
         <div className='bg-red  md:w-1/3' >
           <h1 className='text-6xl md:text-8xl my-5  font-bold text-center md:text-start'>YOUR FEET DESERVE THE BEST</h1>
           <p className='text-xl my-5 md:my-10 font-bold text-center md:text-start'>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
           <div className='text-center md:text-start'>
-          <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Shop Now</button>
-          <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Category</button>
+            <button type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Shop Now</button>
+            <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Category</button>
           </div>
-        <footer className=' my-3 text-xl text-center md:text-start'>
-        <p>Also Available On</p>
-        <div className='flex justify-center md:justify-start md:my-3 gap-12 md:gap-0'>
-        <img src="./src/assets/flipkart.png" alt="" />
-        <img src="./src/assets/amazon.png" alt="" />
-        </div>
-      </footer>
+          <footer className=' my-3 text-xl text-center md:text-start'>
+            <p>Also Available On</p>
+            <div className='flex justify-center md:justify-start md:my-3 gap-12 md:gap-0'>
+              <img src="./src/assets/flipkart.png" alt="" />
+              <img src="./src/assets/amazon.png" alt="" />
+            </div>
+          </footer>
         </div>
         <div className='my-5 text-center flex justify-center' >
-          <img className='h-72 md:h-auto'  src="./src/assets/shoe.png" alt="" />
+          <img className='h-72 md:h-auto' src="./src/assets/shoe.png" alt="" />
         </div>
       </main>
-      
+
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./node_modules/flowbite/**/*.js",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    flowbite.plugin(),
   ]
 }
 
